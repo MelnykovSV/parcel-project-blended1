a70bcbd4cb28e35494d6e0509e919936a123a962
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-01-20 19:10:42 +0000

    Deploying to gh-pages from @ MelnykovSV/parcel-project-blended1@cb578eca6949807eb53ebf2dc9a41cbdc0362b5d 🚀

---
+++ index.6874b1dd.js
@@ -0,0 +1,2 @@
+$(".js-team-slider").slick({prevArrow:'<button type="button" class="team-list__slick-btn">&xlarr;</button>',nextArrow:'<button type="button" class="team-list__slick-btn">&xrarr;</button>',infinite:!0,speed:300,slidesToScroll:1,adaptiveHeight:!0,slidesToShow:3,responsive:[{breakpoint:768,settings:{slidesToShow:2}}]});
+//# sourceMappingURL=index.6874b1dd.js.map
---
+++ index.f2251c2c.js
@@ -0,0 +1,2 @@
+$(".js-team-slider").slick({prevArrow:'<button type="button" class="team-list__slick-btn">&xlarr;</button>',nextArrow:'<button type="button" class="team-list__slick-btn">&xrarr;</button>',infinite:!0,speed:300,slidesToScroll:1,adaptiveHeight:!0,slidesToShow:3,responsive:[{breakpoint:768,settings:{slidesToShow:2}}]});
+//# sourceMappingURL=index.f2251c2c.js.map
